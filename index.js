d1f93dd28be1ea13969a3544ea4625ffd2ed781d
Diego <didipinheiro@Outlook.com>
2021-11-26 15:42:33 -0300

    first version

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById('button').addEventListener('mouseover', mousedentro);
+document.getElementById('button').addEventListener('mouseout', mousefora);
+
+
+var cb = document.getElementById('button2');
+cb.addEventListener('click', coloredclick);
+
+
+
+function coloredclick() {
+    var red = Math.floor(Math.random() * 256)
+    var green = Math.floor(Math.random() * 256)
+    var blue = Math.floor(Math.random() * 256)
+    var cor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    cb.style.backgroundColor = cor;
+}
+
+function mousedentro() {
+    var buttons = document.getElementById('button')
+    buttons.style.backgroundColor = '#515151 ';
+}
+
+function mousefora() {
+    var buttons = document.getElementById('button')
+    buttons.style.backgroundColor = '#626262';
+}   
